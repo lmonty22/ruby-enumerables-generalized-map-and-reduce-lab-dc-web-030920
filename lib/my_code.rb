a340046362b526e83365b7bce37d275acfdd6bb5
Lindsay Montgomery <lindsay.montgomery@olympian.org>
2020-02-09 21:57:09 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map (array)
+  result = []
+  index = 0 
+  while index < array.length do 
+    result.push(yield(array[index]))
+    index += 1 
+  end
+result
+end
+
+def reduce(array, sv= nil)
+if sv 
+  total = sv
+  index = 0 
+else 
+  total = array[0]
+  index = 1 
+end 
+while index < array.length do 
+  total = yield(total, array[index])
+  index += 1 
+end
+total
+end
